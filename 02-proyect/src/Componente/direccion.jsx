ecd75589844a4d3e393a7eed0889a22f404ee65d
Elgember Juracan <elgemberj@gmail.com>
2026-01-26 14:12:31 -0600

    estilos de la direccion

--- 02-proyect/src/Componente/direccion.jsx
+++ 02-proyect/src/Componente/direccion.jsx
@@ -8,19 +8,19 @@ export const Direccion = () => {
     <section className="section__envio">
         <h2 className="envio__title">direcion de envio</h2>
         <div className="envio__informacion">
-            <label htmlFor="name">
+            <label htmlFor="name">Nombre Completo
                 <input className="envio__nombre" type="text" name="nombres__usuario" id="ed" placeholder="Nombres" />
             </label>
             <label htmlFor="firtName">Primer Apellido
-                <input className="envio__nombre" type="text" name="primer-apellido__usuario" id="firtName" placeholder="Primer Apellido" />
+                <input className="envio__apellido" type="text" name="primer-apellido__usuario" id="firtName" placeholder="Primer Apellido" />
             </label>
-            <label htmlFor="lastName">
-                <input className="envio__nombre" type="text" name="segundoApellido" id="lastName" placeholder="Segundo Apellido" />
+            <label htmlFor="lastName">Segundo Apellido
+                <input className="envio__apellido" type="text" name="segundoApellido" id="lastName" placeholder="Segundo Apellido" />
             </label>
         </div>
         <div>
-            <label htmlFor="direccion">
-                <input type="text" name="direccion__usuario" id="direccion" placeholder=" Tu Direccion" />
+            <label htmlFor="direccion">Direccion
+                <input className="envio__direccion" type="text" name="direccion__usuario" id="direccion" placeholder=" Tu Direccion" />
             </label>
         </div>
         <div>
